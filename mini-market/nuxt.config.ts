e4d0373b4ef6f474c6bf210029e3276c0a222eb7
visneviySecret <visneviy.secret@yandex.ru>
2023-04-19 23:45:28 +0300

    meta tags

--- mini-market/nuxt.config.ts
+++ mini-market/nuxt.config.ts
@@ -1,3 +1,9 @@
 export default defineNuxtConfig({
   css: ["~/assets/styles/main.scss"],
+  app: {
+    head: {
+      title: "Ваша корзина",
+      meta: [{ name: "description", content: "Управляй своей корзиной" }],
+    },
+  },
 });
