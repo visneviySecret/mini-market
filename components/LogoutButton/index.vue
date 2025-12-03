170b6617c22b2bd1fed372ad92bd08a51dc27ed8
E. Belousov <visneviy.secret@yandex.ru>
2025-12-03 13:49:19 +0300

    Logout button

---
+++ components/LogoutButton/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="leave-button">
+    <button @click="handleLeave">{{ "<- " }}Выйти из аккаунта</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+const auth = useCookie<string | undefined>(
+  import.meta.env.VITE_RESRESH_TOKEN as string
+);
+
+const handleLeave = () => {
+  auth.value = undefined;
+  navigateTo("/authorize");
+};
+</script>
--- components/NavBar/index.vue
+++ components/NavBar/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <LogoutButton />
     <CartStatus />
   </div>
 </template>
