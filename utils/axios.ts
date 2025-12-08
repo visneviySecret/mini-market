063f517d9ff2e00ed5ca8ec4c027528cf1181ae7
E. Belousov <visneviy.secret@yandex.ru>
2025-12-08 20:59:46 +0300

    Unauthorized redirect

--- utils/axios.ts
+++ utils/axios.ts
@@ -27,6 +27,14 @@ apiClient.interceptors.response.use(
         error.response.data?.message ||
         error.response.data?.error ||
         "Произошла ошибка";
+
+      if (error.response.status === 401) {
+        const auth = useCookie<string | undefined>(
+          import.meta.env.VITE_RESRESH_TOKEN as string
+        );
+        auth.value = undefined;
+        return navigateTo("/authorize");
+      }
       return Promise.reject(new Error(message));
     }
     return Promise.reject(error);
